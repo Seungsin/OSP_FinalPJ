27a3ac89bd22f62778a7c8f0ae1ec78e02520818
Seungsin <62578738+Seungsin@users.noreply.github.com>
2020-06-24 23:55:37 +0900

    실행 환경 설정 v2.1
    실행 환경을 설정하는 shell script입니다.
    수행 환경은 이미 elasticsearch가 백그라운드에 실행되고 있다고 하여 elasticsearch를 굳이 실행시키지는 않았습니다.
    <구현>
    1. 파일 생성(python script, templates, static, uploads파일 들어갈 예정)
    2. Downloads 디렉토리 방문하여 압축 해제 이후 1에서 만든 파일로 이동.
    3. 1에서 만든 파일로 이동하여 FLASK의 환경변수를 first_python.py로 설정
    4. flask run으로 실행
    -> 본 스크립트 실행 이후 chrom 브라우저에서 "127.0.0.1/5000"입력 시 정상 구동 됩니다.

--- start.sh
+++ start.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+mkdir Final_team1
+
+cd Downloads
+mkdir OSP_FinProj_team1
+unzip OSP_FinProj_team1.zip -d ./OSP_FinProj_team1
+cd OSP_FinProj_team1
+mv ./templates ../../Final_team1
+mv ./static ../../Final_team1
+mv ./uploads ../../Final_team1
+mv first_python.py ../../Final_team1
+mv Analysis.py ../../Final_team1
+
 cd
-mkdir OSP_Final
-cd OSP_Final
-mkdir templates
-mkdir uploads
-cd
-cd OSP_FinalProject
-mv first_python.py ../OSP_Final/
-mv HTMLPage1.html ../OSP_Final/templates/
-cd ../OSP_Final/
-chmod 755 first_python.py
+cd Final_team1
 export FLASK_APP=first_python.py
-./first_python.py
+flask run 
