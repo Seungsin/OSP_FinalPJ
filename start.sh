1e631cf1f007245c142cc6f80988c4c966b4cbe8
Seungsin <62578738+Seungsin@users.noreply.github.com>
2020-06-25 10:07:39 +0900

    실행 환경 설정 v2.2
    LMS질의 응답 참고 한 shell 실행
    
    1. LMS 압축파일 다운로드
    2. 압축해제
    3. 압축해제폴더로 이동
    4. 쉘스크립트 파일 실행 ->여기서부터 쉘스크립트가 실행됨
    5. 크롬 웹브라우져 실행 및 동작
    
    4번부터 shell이 실행되므로 이전에 압축을 푸는 과정을 없애도 됩니다. 따라서 그 부분을 없앴습니다:)

--- start.sh
+++ start.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-mkdir Final_team1
+mkdir ../../Final_team1
 
-cd Downloads
-mkdir OSP_FinProj_team1
-unzip OSP_FinProj_team1.zip -d ./OSP_FinProj_team1
-cd OSP_FinProj_team1
 mv ./templates ../../Final_team1
 mv ./static ../../Final_team1
 mv ./uploads ../../Final_team1
